5c2f92eaee5d19a67f8fd573ac90dd840e45c46f
RamonLGDev <rlg.front.dev@gmail.com>
2024-12-13 18:43:22 +0100

    add table

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
-
+df = pd.read_csv('data_small/stations.txt', skiprows=17)
+stations = df[['STAID', 'STANAME                                 ']]
+stations = stations.to_html()
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', data=stations)
 
 @app.route('/api/v1/<station>/<date>')
 def api(station, date):
-    temp = 23
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"   
+    # df = pd.read_csv(filename, skiprows=20)   
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])  
+    temp = df.loc[df["    DATE"] == date]['   TG'].squeeze()/10
     return {'station':station, 'date':date, 'temperature': temp}
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
